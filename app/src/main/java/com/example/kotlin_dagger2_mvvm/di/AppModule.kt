be0accb28684162f55d4737fdfbeab3192aa6045
Kadir Kuruca <kadirkuruca572@gmail.com>
2021-04-22 10:51:37 +0300

    Starting the viewmodel injection

--- app/src/main/java/com/example/kotlin_dagger2_mvvm/di/AppModule.kt
+++ app/src/main/java/com/example/kotlin_dagger2_mvvm/di/AppModule.kt
@@ -9,11 +9,13 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.kotlin_dagger2_mvvm.R
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppModule {
 
     companion object{
+        @Singleton
         @Provides
         fun provideRequestOption() : RequestOptions{
             return RequestOptions
@@ -21,12 +23,14 @@ class AppModule {
                 .error(R.drawable.white_background)
         }
 
+        @Singleton
         @Provides
         fun provideGlideInstance(application: Application, requestOptions: RequestOptions) : RequestManager{
             return Glide.with(application)
                 .setDefaultRequestOptions(requestOptions)
         }
 
+        @Singleton
         @Provides
         fun provideAppDrawable(application: Application) : Drawable{
             return ContextCompat.getDrawable(application, R.drawable.logo)!!
